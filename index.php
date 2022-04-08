379077f0d8df4c902c5c3c75e89a77fd703789e1
Dylan Germann <dev.dyger@gmail.com>
2022-04-08 11:42:43 +0200

    move the instance of Article of index.php in HomeController

--- index.php
+++ index.php
@@ -6,12 +6,11 @@ use App\Model\Article;
 use App\Services\TwigService;
 use App\Controller\HomeController;
 
-$article = new Article();
 
 var_dump($_SERVER);
 if (str_ends_with($_SERVER['PHP_SELF'], "index.php")) {
     $homeController = new homeController();
-    $homeController->showHome($article->getArticlesByNumber(6));
+    $homeController->showHome();
 }
 
 
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,25 +3,30 @@
 declare(strict_types=1);
 namespace App\Controller;
 
+use App\Model\Article;
 use App\Services\TwigService;
 
 class HomeController
 {
     private TwigService $twig;
+    private const HOME_ARTICLES_NUMBER = 6;
     public function __construct()
     {
         $this->twig = TwigService::getInstance();
+        $this->articles = new Article();
+
     }
-    public function showHome(array $articles)
+    public function showHome()
     {
+
         echo $this->twig->getTwig()->render("home.html.twig", [
-            "articles" => $articles
+            "articles" => $this->articles->getArticlesByNumber(self::HOME_ARTICLES_NUMBER)
         ]);
     }
-    public function showBlog(array $articles)
+    public function showBlog()
     {
         echo $this->twig->getTwig()->render("blog.html.twig", [
-            "articles" => $articles
+            "articles" => $this->articles
         ]);
     }
 }
