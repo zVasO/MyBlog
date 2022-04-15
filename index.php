e0fdffc21b4cd007636edd8341e4af462a1fb507
Dylan Germann <dev.dyger@gmail.com>
2022-04-15 08:07:23 +0200

    on attribue 0 si aucun id existe

--- index.php
+++ index.php
@@ -11,12 +11,12 @@ use App\Controller\HomeController;
 //on recupere les parametre de notre url
 $components = parse_url($_SERVER['REQUEST_URI']);
 parse_str($components['query'], $results);
-
+isset($results["id"]) ? $id = (int)$results["id"] : $id = 0;
 
 //switch en fonction de l'uri, on donne la page demandée
 switch($_SERVER['QUERY_STRING']) {
     case "/article":
-        (new ArticleController())->showArticle((int)$results["id"]);
+        (new ArticleController())->showArticle($id);
         break;
     case "/blog":
         (new BlogController())->showBlog();
