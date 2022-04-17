f98d074965f174a2ddf1c6c2bb9d3f09c564b84a
Dylan Germann <dev.dyger@gmail.com>
2022-04-17 18:16:14 +0200

    creation d'une requete qui retourne le nombre d'utilisateurs + integration dans le dashboard

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,22 +4,33 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Model\ArticleModel;
+use App\Model\CommentModel;
+use App\Model\UserModel;
 use App\Services\TwigService;
 
 class AdminController
 {
 
     private TwigService $twig;
+    private ArticleModel $articles;
+    private UserModel $users;
+    private CommentModel $comments;
 
     public function __construct()
     {
         $this->twig = TwigService::getInstance();
+        $this->articles = new ArticleModel();
+        $this->users = new UserModel();
+        $this->comments = new CommentModel();
     }
 
     public function showDashboard()
     {
+        //todo donner les nombres d'inscrit, d'articles, de commentaires et de commentaire en attente
         echo $this->twig->getTwig()->render("admin/dashboard.html.twig", [
-            "session" => $_SESSION
+            "session" => $_SESSION,
+            "totalUsers" => $this->users->countTotalUsers()
         ]);
     }
 
